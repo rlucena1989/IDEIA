178710ccf45184ef696573478ead0c37a2b18c40
IDEIA Dev <dev@ideia.ai>
2026-07-30 02:40:00 -0300

    Remove temporary and obsolete files from git tracking

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'ci', 'build']],
-    'scope-empty': [1, 'never'],
-    'subject-case': [2, 'always', 'lower-case'],
-  },
-};
